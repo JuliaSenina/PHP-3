3bb5f5f39d6883c38ea4eba56fdce2c9265154ca
Сенина Юлия Александровна <y.senina@ens.ru>
2023-03-15 12:43:27 +0300

    commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+echo 'Введите первое число ';
+fscanf(STDIN, "%d\n", $number1);
+while (is_int($number1) === false) {
+  fwrite(STDERR, 'Введите, пожалуйста, число ' . PHP_EOL);
+  fscanf(STDIN, "%d\n", $number1);
+};
+
+echo 'Введите второе число ';
+fscanf(STDIN, "%d\n", $number2);
+while (is_int($number2) === false) {
+  fwrite(STDERR, 'Введите, пожалуйста, число ' . PHP_EOL);
+  fscanf(STDIN, "%d\n", $number2);
+};
+
+while ($number2 === 0) {
+  fwrite(STDERR, 'Делить на 0 нельзя' . PHP_EOL);
+  fscanf(STDIN, "%d\n", $number2);
+};
+
+fwrite(STDOUT, 'Результат ' . $number1 / $number2 . PHP_EOL);
